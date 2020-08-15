f1f335e3f8c26e9855c8ec9370adef859c9a273c
FloydWatson <floydbcseit@gmail.com>
2020-08-15 13:05:47 +1200

    update for dummy data

--- src/store/modules/data.js
+++ src/store/modules/data.js
@@ -1,30 +1,22 @@
 const state = {
-  // your list of furniture
-  furniture: [
-    { id: 1, name: "Kanapa Sydney", dim1: "2,25m", dim2: "1,45m x 1,95m" },
-    { id: 2, name: "Kanapa Alex", dim1: "1,95m", dim2: "1,45m x 1,95m" },
-    { id: 3, name: "Blah Alex", dim1: "1,95m", dim2: "1,45m x 1,95m" },
-  ],
+
+  dummyData: [
+
+  ]
 };
 
 const getters = {
-  // getter for your furniture
-  allFurniture: (state) => state.furniture,
+
+  allDummyData: (state) => state.dummyData,
 };
 
 const actions = {
-  // have a getter here that would call an API to return your furniture objects. Here is an example using axios
-  //   async fetchFurniture({ commit }) {
-  //     const { data }  = await http.get(
-  //       `/furniture`
-  //     );
-  //     commit("setFurniture", data);
-  //   }
+
 };
 
 const mutations = {
-  // set your furniture after you have created it
-  setFurniture: (state, furniture) => (state.furniture = furniture),
+
+  setFurniture: (state, dummyData) => (state.dummyData = dummyData),
 };
 
 export default {
